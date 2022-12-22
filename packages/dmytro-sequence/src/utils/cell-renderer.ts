adf8d358434128fe131df1a6dbb96424ae5a3b24
Dmitry Kovalyov <laykdimon278@gmail.com>
2022-12-22 16:46:43 +0200

    dkovalyov/exercises: fixed cell renderer

--- packages/dmytro-sequence/src/utils/cell-renderer.ts
+++ packages/dmytro-sequence/src/utils/cell-renderer.ts
@@ -6,20 +6,30 @@ export class NucleotideBoxCellRenderer extends DG.GridCellRenderer {
 
   render(g: CanvasRenderingContext2D, x: number, y: number, w: number, h: number,
     gridCell: DG.GridCell, cellStyle: DG.GridCellStyle) {
+    g.save();
+    g.beginPath();
+    g.rect(x, y, w, h);
+    g.clip();
     g.font = '16px monospace';
     g.textBaseline = 'top';
+    g.restore();
 
     const complementDictionary: {[letterToPaint: string]: string} = {
       'a': 'green',
       't': 'red',
       'g': 'black',
       'c': 'blue',
+      'r': 'turquoise',
+      'n': 'turquoise',
+      'd': 'burgundy',
     };
 
     //"grid" parameteres of dna rendering in cell
     const rowsAmount = 4;
     const dy = 20;
     const xBegin = x;
+    const xPadding = 3;
+    const yPadding = 15;
 
     let i = 0;
     for (let n = 1; n <= rowsAmount; n++) {
@@ -27,7 +37,7 @@ export class NucleotideBoxCellRenderer extends DG.GridCellRenderer {
       for (; i < Math.ceil(gridCell.cell.value.length * (n / rowsAmount)); i++) {
         const ch = gridCell.cell.value.charAt(i);
         g.fillStyle = complementDictionary[ch.toLowerCase()];
-        g.fillText(ch, x, y);
+        g.fillText(ch, x + xPadding, y + yPadding);
         x += g.measureText(ch).width;
       }
       y += dy;
